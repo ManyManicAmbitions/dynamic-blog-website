c4ba59de834c977976cdb525a6363539f7be1b9a
ManyManicAmbitions <russelltyrel9@gmail.com>
2025-04-01 17:30:17 -0400

    Added event listner for loading posts and moving save button to new-post.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-const postTitleInput = document.getElementById("post-title");
-const postContentInput = document.getElementById("post-content");
-const saveBtn = document.getElementById("save-btn");
-const postList = document.getElementById("post-list");
-
-saveBtn.addEventListener("click", savePost);
-
-function savePost(){
-    const title = postTitleInput.value;
-    const content = postContentInput.value;
-        if (title && content){
-            const newPost = {
-                title: title,
-                content: content
-            };
-            let storedPosts = JSON.parse(localStorage.getItem("blogPosts")) || [];
-                storedPosts.push(newPost);
-            
-                localStorage.setItem("blogPosts", JSON.stringify(storedPosts));
-
-                postTitleInput.value = "";
-                postContentInput.value = "";
-
-            loadBlogPosts();
-        }
-}
-
 function loadBlogPosts(){
     const postList = document.getElementById("post-list");
     const storedPosts = JSON.parse(localStorage.getItem("blogPosts")) || [];
@@ -40,3 +13,4 @@ function loadBlogPosts(){
         postList.appendChild(listItem);
     });
 }
+window.addEventListener("load", loadBlogPosts);
